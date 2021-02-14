588910626ec33203ab7d27c871517a0539fe121f
Adam <adam.j.major@gmail.com>
2021-02-13 21:24:46 -0800

    Add two more tests to test_examples.py

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -8,6 +8,7 @@ class NameOfFunctionBeingTested(unittest.TestCase):
     def test_caseName(self):
         self.assertTrue(True)
 
+    #comparing equality of valu
     def test_sumSuccess(self):
         #do some things to get the actual
         actual = sum([1,3,4])
@@ -20,17 +21,27 @@ class NameOfFunctionBeingTested(unittest.TestCase):
         expected = 7
         self.assertEqual(actual, expected)
 
+    #comparing boolean True
     def test_isEven(self):
         #do some things to get the actual
         numberToTest = 6
         actual = numberToTest % 2 == 0
         self.assertTrue(actual)
 
+    #comparing boolean False
     def test_isEvenFalse(self):
         #do some things to get the actual
         numberToTest = 7
         actual = numberToTest % 2 == 0
         self.assertFalse(actual)
+
+    #comparing lists
+    def test_isSameList(self):
+        self.assertListEqual([1,2], [1,2])
+
+    def test_isSameListFail(self):
+        self.assertListEqual([1,2], [1,2,3])
+
 
     def test_split(self):
         s = 'hello world'
